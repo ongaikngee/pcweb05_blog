e8999a11d1762c596b69fac4fb520087b6034d5f
Glippy <jonathanong@iMac.local>
2020-10-26 17:16:05 +0800

    changing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
                     @foreach ($blogs as $blog)
 
                         <div class="row m-4">
-                            <div class="h2"><img width="50" src="/storage/{{ $blog->image }}">{{ $blog->name }}</div>
+                            <div class="h2"><img width="50" src="storage/{{ $blog->image }}">{{ $blog->name }}</div>
                             <div class="col-12 border border-primary p-4">
 
                                 <div class="h2">{{ $blog->title }}</div>
@@ -23,7 +23,7 @@
                                 @endif
                                 <div class="my-4">{{ $blog->content }}</div>
                                 @if ($blog->image != null)
-                                    <div><img height="300" src="/storage/{{ $blog->b_image }}"></div>
+                                    <div><img height="300" src="/{{ $blog->b_image }}"></div>
                                 @endif
                             </div>
                         </div>
